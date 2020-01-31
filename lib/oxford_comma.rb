dc024b91337c21b2e0a533d153f7bb7d499686c0
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-31 21:18:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
     return array[0]
-    elsif array.length == 2
+  elsif array.length == 2
     return array.join(" and ")
-    elsif array.length == 3
+  elsif array.length >= 3
     return array[0..-2].join(", ") + ", and " + array[-1]
-  else array.count{|arr| arr.length > 3}
-    return array.count.join(", ") + ", and " 
+  # else array.count{|arr| arr.length > 3}
+  #   return array.count.join(", ") + ", and " 
   end
 end
