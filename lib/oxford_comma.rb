1b643143cd9750d0b8cf861a70b35582cfda967d
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-31 21:11:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
     elsif array.length == 3
     return array[0..-2].join(", ") + ", and " + array[-1]
   else array.count{|arr| arr.length > 3}
-    return array[0..-1].join(", ") + ", and " 
+    return array.count.join(", ") + ", and " 
   end
 end
